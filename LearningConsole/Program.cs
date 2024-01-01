c678ea64b3fb85c154c32c7cdf4964476a3e8a5b
dominicod <dominicodonnell99@gmail.com>
2024-01-01 17:48:55 -0600

    Fix small error

--- LearningConsole/Program.cs
+++ LearningConsole/Program.cs
@@ -4,7 +4,7 @@ var solution = new MaximumSubarraySumClass();
 
 Console.WriteLine("Input: ([ 1,5,4,2,9,9,9 ], 3)");
 Console.WriteLine("Expected Output: 15");
-Console.WriteLine("Result: " + solution.MaximumSubarraySum(new[] { 1,5,4,2,9,9,9 }, 5));
+Console.WriteLine("Result: " + solution.MaximumSubarraySum(new[] { 1,5,4,2,9,9,9 }, 3));
 
 Console.WriteLine("Input: ([4,4,4], 3)");
 Console.WriteLine("Expected Output: 0");
